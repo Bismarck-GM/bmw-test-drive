951d890fe6d5171cd5c2a52d3f6b2af74b76663f
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-05-27 08:31:11 -0300

    Edit App.js. Add basic routing and Navbar passing props.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import Main from './container/Main';
+import Navbar from './container/Navbar';
+import Home from './components/Home';
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    action: {
+      selected: '#97BF0F',
+    },
+  },
+  typography: {
+    fontFamily: '"Cabin", "Roboto", "Helvetica", "Arial", sans-serif',
+  },
+});
+
+const App = () => {
+  const [drawer, setDrawer] = useState(true);
+  // const [response, setResponse] = useState(null);
+
+  const handleDrawerOpen = () => {
+    setDrawer(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawer(false);
+  };
+
+  // useEffect(async () => {
+  //   const response = await fetch('http://127.0.0.1:3000/api/v1/dealerships').then((res) => res.json());
+  //   setResponse(response);
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Navbar
+          open={drawer}
+          handleDrawerOpen={handleDrawerOpen}
+          handleDrawerClose={handleDrawerClose}
+        />
+        <Switch>
+          <Main open={drawer}>
+            <Route path="/" exact component={Home} />
+          </Main>
+        </Switch>
+
+      </ThemeProvider>
+    </Router>
   );
-}
+};
 
 export default App;
