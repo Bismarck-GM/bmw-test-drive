38860d321966b223d52b7aa0cd163aa0dbd74b44
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-05-27 14:16:17 -0300

    Edit navbar. Add button for loged user. Add login button.

--- src/container/Navbar.js
+++ src/container/Navbar.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -8,10 +9,14 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Tooltip from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
 import { ReactComponent as BmwLogo } from '../logo.svg';
+import LoginModal from '../components/LoginModal';
 
 const drawerWidth = 240;
 
@@ -21,9 +26,13 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     position: 'absolute',
-    top: '0',
-    left: '50',
-    padding: '30px',
+    top: '30px',
+    left: '50px',
+  },
+  menuButtonUser: {
+    position: 'absolute',
+    top: '30px',
+    left: '120px',
   },
   hide: {
     display: 'none',
@@ -34,7 +43,6 @@ const useStyles = makeStyles((theme) => ({
   },
   drawerPaper: {
     width: drawerWidth,
-    // backgroundColor: 'grey',
   },
   drawerHeader: {
     display: 'flex',
@@ -46,6 +54,11 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: '150px',
   },
+  logo: {
+    '& path:nth-child(1)': {
+      fill: theme.palette.type === 'light' ? '#B2B2B2' : '#fff',
+    },
+  },
   drawerCloserContainer: {
     width: '100%',
     display: 'flex',
@@ -54,32 +67,45 @@ const useStyles = makeStyles((theme) => ({
   navList: {
     padding: theme.spacing(1, 0, 1, 4),
   },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary,
+  },
 }));
 
-const Navbar = ({ open, handleDrawerOpen, handleDrawerClose }) => {
+const Navbar = ({
+  open,
+  handleDrawerOpen,
+  handleDrawerClose,
+}) => {
   const classes = useStyles();
-  // const [open, setOpen] = React.useState(true);
+  const { pathname: location } = useLocation();
 
-  // const handleDrawerOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleDrawerClose = () => {
-  //   setOpen(false);
-  // };
+  const [openModal, setOpenModal] = React.useState(false);
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={handleDrawerOpen}
-        // edge="start"
-        className={clsx(classes.menuButton, open && classes.hide)}
-      >
-        <MenuIcon fontSize="large" />
-      </IconButton>
+      <Tooltip title="Open Menu" arrow>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          className={clsx(classes.menuButton, open && classes.hide)}
+        >
+          <MenuIcon fontSize="large" />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Open User" arrow>
+        <IconButton
+          color="inherit"
+          className={clsx(classes.menuButtonUser, open && classes.hide)}
+        >
+          <AccountCircleIcon fontSize="large" />
+        </IconButton>
+      </Tooltip>
       <Drawer
         className={classes.drawer}
         variant="persistent"
@@ -93,7 +119,7 @@ const Navbar = ({ open, handleDrawerOpen, handleDrawerClose }) => {
           <div
             className={classes.logoContainer}
           >
-            <BmwLogo width="100%" height="100%" />
+            <BmwLogo width="100%" height="100%" className={classes.logo} />
           </div>
           <div
             className={classes.drawerCloserContainer}
@@ -105,20 +131,37 @@ const Navbar = ({ open, handleDrawerOpen, handleDrawerClose }) => {
         </div>
         <Divider />
         <List className={classes.navList}>
-          <ListItem button selected>
-            <ListItemText primary="MODELS" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="LIFESTYLE" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="SHOP" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="TEST DRIVE" />
-          </ListItem>
+          <Link to="/models" className={classes.link}>
+            <ListItem button selected={location === '/models'}>
+              <ListItemText primary="MODELS" />
+            </ListItem>
+          </Link>
+          <Link to="/lifestyle" className={classes.link}>
+            <ListItem button selected={location === '/lifestyle'}>
+              <ListItemText primary="LIFESTYLE" />
+            </ListItem>
+          </Link>
+          <Link to="/shop" className={classes.link}>
+            <ListItem button selected={location === '/shop'}>
+              <ListItemText primary="SHOP" />
+            </ListItem>
+          </Link>
+          <Link to="/testdrive" className={classes.link}>
+            <ListItem button selected={location === '/testdrive'}>
+              <ListItemText primary="TEST DRIVE" />
+            </ListItem>
+          </Link>
         </List>
         <Divider />
+        <Button onClick={handleOpenModal} primary>
+          Log In
+        </Button>
+        <LoginModal
+          openModal={openModal}
+          handleCloseModal={handleCloseModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        />
       </Drawer>
     </div>
   );
