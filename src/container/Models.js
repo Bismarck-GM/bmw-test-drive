8f1dd15d849b3a8c63f47535c7b46350a66b139c
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-01 12:41:23 -0300

    Edit Models.js. Add logic for displaying CarFamilies. Connection to Redux store.

--- src/container/Models.js
+++ src/container/Models.js
@@ -1,14 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   makeStyles,
-  // Container,
   Typography,
 } from '@material-ui/core';
+// import Divider from '@material-ui/core/Divider';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation } from 'swiper/core';
 import { GrCaretNext, GrCaretPrevious } from 'react-icons/gr';
+import { fetchCarFamilies } from '../redux/actions';
+// import CarFamilySlide from '../components/CarFamilySlide';
 import 'swiper/swiper.min.css';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import 'swiper/components/navigation/navigation.scss';
 
 const useStyles = makeStyles(() => ({
@@ -35,34 +38,86 @@ SwiperCore.use([Navigation]);
 
 const Models = () => {
   const classes = useStyles();
+  const { loading, cars } = useSelector((state) => state.carFamily);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    if (loading) {
+      dispatch(fetchCarFamilies());
+    }
+  }, []);
+
   return (
     <div className={classes.wrapper}>
       <Typography variant="h3">
         LATEST MODELS
       </Typography>
-      <Typography variant="subtitle1">
+      <Typography variant="subtitle1" gutterBottom>
         Select from any of the Models
       </Typography>
+      <hr style={{
+        border: 'dotted 5px',
+        width: '10%',
+        borderStyle: 'none none dotted none',
+        color: 'grey',
+        opacity: '30%',
+        marginTop: '2rem',
+        marginBottom: '1rem',
+      }}
+      />
       <div className={classes.swiperWrapper}>
         <Swiper
           className="mySwiper"
-          slidesPerView={2}
-          spaceBetween={50}
+          slidesPerView={1}
+          spaceBetween={0}
           resizeObserver
           navigation={{
             nextEl: '.swiperNext',
             prevEl: '.swiperPrevious',
           }}
+          breakpoints={{
+            769: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+          }}
         >
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 1</SwiperSlide>
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 2</SwiperSlide>
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 3</SwiperSlide>
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 4</SwiperSlide>
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 5</SwiperSlide>
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 6</SwiperSlide>
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 7</SwiperSlide>
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 8</SwiperSlide>
-          <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 9</SwiperSlide>
+          {
+            loading
+              ? (
+                <>
+                  <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 1</SwiperSlide>
+                  <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 2</SwiperSlide>
+                  <SwiperSlide style={{ width: '200px', height: '400px', backgroundColor: 'red' }}>Slide 3</SwiperSlide>
+                </>
+              )
+              : (
+                cars.map((car) => (
+                  <SwiperSlide key={car.id} style={{ width: '100%', position: 'relative' }}>
+                    <img src={car.img_thumb} alt={car.name} style={{ width: '100%' }} />
+                    <Typography variant="h4" align="center" style={{ position: 'absolute', top: '10px', left: '10px' }}>
+                      {car.shortname}
+                    </Typography>
+                    <Typography variant="h5" align="center" gutterBottom>
+                      {car.name}
+                    </Typography>
+                    <hr style={{
+                      border: 'dotted 5px',
+                      width: '30%',
+                      borderStyle: 'none none dotted none',
+                      color: 'grey',
+                      opacity: '30%',
+                      marginTop: '1rem',
+                      marginBottom: '1rem',
+                    }}
+                    />
+
+                    <Typography variant="subtitle2" align="center">
+                      {car.description}
+                    </Typography>
+                  </SwiperSlide>
+                ))
+              )
+          }
         </Swiper>
         <div className="swiperNext">
           <GrCaretNext />
