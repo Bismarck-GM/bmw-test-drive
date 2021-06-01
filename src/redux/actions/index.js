8366bc9b60e66a6d45a9e8433e64721fc987f1cf
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-01 12:36:46 -0300

    Add new Types and Actions for redux. uiModal, uiSnackBar, login and register.

---
+++ src/redux/actions/index.js
@@ -0,0 +1,76 @@
+import Api, {
+  GET_CAR_FAMILIES,
+  LOGIN,
+  REGISTER,
+} from '../../api';
+import * as types from './types';
+
+export const createCarFamilies = (carFamily) => ({
+  type: types.SET_CAR_FAMILIES,
+  payload: carFamily,
+});
+
+export const snackBar = (type, message) => ({
+  type,
+  payload: message,
+});
+
+export const loginUser = (data) => ({
+  type: types.LOGIN_USER,
+  payload: data,
+});
+
+export const openModal = (type) => ({
+  type: types.MODAL_OPEN,
+  payload: type,
+});
+
+export const closeModal = () => ({ type: types.MODAL_CLOSE });
+
+export const formError = (message) => ({
+  type: types.FORM_ERROR,
+  payload: message,
+});
+
+export const toggleFormLoading = () => ({ type: types.FORM_LOADING });
+
+export const snackBarClear = () => ({ type: types.SNACKBAR_CLEAR });
+
+export const fetchCarFamilies = () => async (dispatch) => {
+  try {
+    const { data } = await Api({ ...GET_CAR_FAMILIES() });
+    dispatch(createCarFamilies(data));
+  } catch (err) {
+    dispatch(snackBar(types.SNACKBAR_ERROR, 'Could not connect to back-end.'));
+  }
+};
+
+export const logIn = (formInputs) => async (dispatch) => {
+  dispatch(toggleFormLoading());
+  try {
+    const { data } = await Api({ ...LOGIN(), data: { ...formInputs } });
+    dispatch(loginUser(data));
+    dispatch(snackBar(types.SNACKBAR_SUCCESS, `Welcome back ${data.username}.`));
+    dispatch(closeModal());
+  } catch (err) {
+    console.log(err.response);
+    dispatch(toggleFormLoading());
+    dispatch(formError(err.response.data.error));
+    dispatch(snackBar(types.SNACKBAR_ERROR, err.response.data.error));
+  }
+};
+
+export const register = (formInputs) => async (dispatch) => {
+  dispatch(toggleFormLoading());
+  try {
+    const { data } = await Api({ ...REGISTER(), data: { ...formInputs } });
+    dispatch(loginUser(data));
+    dispatch(snackBar(types.SNACKBAR_SUCCESS, `Welcome ${data.username}.`));
+    dispatch(closeModal());
+  } catch (err) {
+    console.log(err.response);
+    dispatch(toggleFormLoading());
+    dispatch(formError(err.response.data.error));
+    dispatch(snackBar(types.SNACKBAR_ERROR, err.response.data.error));
+  }
+};
--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,4 +1,22 @@
 export const TOGGLE_ADMIN = 'TOGGLE_ADMIN';
-export const TOGGLE_ADMIS = 'TOGGLE_ADMINS';
 
 export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
+
+export const SET_CAR_FAMILIES = 'SET_CAR_FAMILIES';
+
+export const GET_CARS = 'GET_CARS';
+
+export const GET_DEALERSHIPS = 'GET_DEALERSHIPS';
+
+export const GET_USER_APPOINTMENTS = 'GET_USER_APPOINTMENTS';
+export const POST_USER_APPOINTMENTS = 'POST_USER_APPOINTMENTS';
+
+export const SNACKBAR_ERROR = 'SNACKBAR_ERROR';
+export const SNACKBAR_SUCCESS = 'SNACKBAR_SUCCESS';
+export const SNACKBAR_CLEAR = 'SNACKBAR_CLEAR';
+
+export const MODAL_OPEN = 'MODAL_OPEN';
+export const MODAL_CLOSE = 'MODAL_CLOSE';
+export const FORM_LOADING = 'FORM_LOADING';
+export const FORM_ERROR = 'FORM_ERROR';
