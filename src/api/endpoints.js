90f9e69fb8f5c6837898a099cf8a3862a5ed2764
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-05 01:42:33 -0300

    Edit Api endpoints. Add for Cars and CarFamily.

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,8 +2,8 @@ export const LOGIN = () => ({ method: 'POST', url: '/login' });
 export const REGISTER = () => ({ method: 'POST', url: '/register' });
 
 export const GET_CAR_FAMILIES = () => ({ method: 'GET', url: '/models' });
+export const GET_CARS = () => ({ method: 'GET', url: '/cars' });
 export const GET_HOUSE = (id) => ({ method: 'GET', url: `/houses/${id}` });
-export const GET_HOUSE_TYPES = () => ({ method: 'GET', url: '/house_types' });
 export const GET_FAVOURITES = () => ({ method: 'GET', url: '/favourites' });
 export const FAVOURITE_HOUSE = (id) => ({ method: 'POST', url: `/houses/${id}/favourite` });
 export const UNFAVOURITE_HOUSE = (id) => ({ method: 'POST', url: `/houses/${id}/unfavourite` });
