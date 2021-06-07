1340c440fd03565df8a0d86847a95374fd33c158
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-07 20:21:37 -0300

    Edit Api/Index. Add functionality to fetch from LocalStore.

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,6 @@ import Axios from 'axios';
 
 export * from './endpoints';
 
-const user = JSON.parse(window.localStorage.getItem('user'));
-
 const api = Axios.create({
   baseURL: 'http://localhost:3000/api/v1/',
   headers: {
@@ -11,6 +9,13 @@ const api = Axios.create({
   },
 });
 
-api.defaults.headers.common.Authorization = `Bearer ${user ? user.token : ''}`;
+export const setToken = () => {
+  const user = JSON.parse(window.localStorage.getItem('user'));
+  if (user) {
+    api.defaults.headers.common.Authorization = `Bearer ${user.token}`;
+  }
+};
+
+setToken();
 
 export default api;
