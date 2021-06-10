c8cf50049cde078ba26617a67d783c7de31eb5a8
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-10 19:01:10 -0300

    Edit endpoints. Add DELETE for Appointments.

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -5,4 +5,5 @@ export const GET_CAR_FAMILIES = () => ({ method: 'GET', url: '/models' });
 export const GET_CARS = () => ({ method: 'GET', url: '/cars' });
 export const GET_APPOINTMENTS = () => ({ method: 'GET', url: '/appointments' });
 export const POST_APPOINTMENT = () => ({ method: 'POST', url: '/appointments' });
+export const DELETE_APPOINTMENT = () => ({ method: 'DELETE', url: '/appointments' });
 export const GET_DEALERSHIPS = () => ({ method: 'GET', url: '/dealerships' });
