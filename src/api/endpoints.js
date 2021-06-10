87af7fb004af5f48f8bfe17e54da696db83c12c2
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-09 21:05:26 -0300

    Edit endpoints.js. Add POST for appointments.

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -4,6 +4,5 @@ export const REGISTER = () => ({ method: 'POST', url: '/register' });
 export const GET_CAR_FAMILIES = () => ({ method: 'GET', url: '/models' });
 export const GET_CARS = () => ({ method: 'GET', url: '/cars' });
 export const GET_APPOINTMENTS = () => ({ method: 'GET', url: '/appointments' });
-export const GET_HOUSE = (id) => ({ method: 'GET', url: `/houses/${id}` });
-export const FAVOURITE_HOUSE = (id) => ({ method: 'POST', url: `/houses/${id}/favourite` });
-export const UNFAVOURITE_HOUSE = (id) => ({ method: 'POST', url: `/houses/${id}/unfavourite` });
+export const POST_APPOINTMENT = () => ({ method: 'POST', url: '/appointments' });
+export const GET_DEALERSHIPS = () => ({ method: 'GET', url: '/dealerships' });
