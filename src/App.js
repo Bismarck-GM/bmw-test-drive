3f81432096a3b981a52e12559f86d881f2c2362f
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-10 19:00:48 -0300

    Edit App.js. Exctracted theming.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import Main from './components/Main';
@@ -18,45 +18,7 @@ import LifeStyle from './components/LifeStyle';
 import Shop from './components/Shop';
 import Modal from './container/Modal';
 import TestDrive from './container/TestDrive';
-
-const darkTheme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: 'rgb(22,88,142)',
-      dark: '#C82D2B',
-    },
-    secondary: {
-      main: '#81C4FF',
-    },
-    action: {
-      selected: '#C82D2B',
-    },
-  },
-  typography: {
-    fontFamily: '"Cabin", "Roboto", "Helvetica", "Arial", sans-serif',
-    fontWeightRegular: 600,
-  },
-});
-
-const lightTheme = createMuiTheme({
-  palette: {
-    type: 'light',
-    primary: {
-      main: '#81C4FF',
-      dark: 'rgb(22,88,142)',
-    },
-    secondary: {
-      main: 'rgb(22,88,142)',
-    },
-    action: {
-      selected: '#81C4FF',
-    },
-  },
-  typography: {
-    fontFamily: '"Cabin", "Roboto", "Helvetica", "Arial", sans-serif',
-  },
-});
+import { darkTheme, lightTheme } from './Theme';
 
 const App = () => {
   const [theme, setTheme] = useState(darkTheme);
